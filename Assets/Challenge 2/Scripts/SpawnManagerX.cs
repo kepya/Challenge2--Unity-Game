4f07cb0767bdfd5cd806e940aa5eab92bd6b98e2
kepya <58195511+kepya@users.noreply.github.com>
2022-09-06 23:03:35 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    public GameObject dogPrefab;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int spawnInterval = Random.Range(3, 6);
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomDog", startDelay, 3);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomDog()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), 0, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(dogPrefab, spawnPos, dogPrefab.transform.rotation);
+    }
+
+}
